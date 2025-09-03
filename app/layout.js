c765d2cb5f173bdb96a22ac577ad6ac7ee362b21
shivateja-droid <pabbojushivateja404@gmail.com>
2025-09-04 00:57:13 +0530

    fix: tailwind safelist and layout update

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Sessionwrapper>
       <body className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" >
+      <Sessionwrapper>
         <Navbar />
         {children}
-        
-      </body>
       </Sessionwrapper>
+      </body>
     </html>
   );
 }
