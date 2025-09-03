ee5405413698f0d8c4016c31465fb911f6085581
shivateja-droid <pabbojushivateja404@gmail.com>
2025-09-03 22:42:11 +0530

    Content-Security-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply CSP globally
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: blob:;
+              connect-src 'self'
+                https://api.github.com
+                https://github.com
+                https://accounts.google.com
+                https://api.razorpay.com;
+              frame-src 'self'
+                https://github.com
+                https://accounts.google.com
+                https://checkout.razorpay.com;
+            `.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
